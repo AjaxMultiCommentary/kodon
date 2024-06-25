e7abdd005f2f9cc6e1f0938c2d28929713d77608
Charles Pletcher <pletcher@users.noreply.github.com>
2024-06-25 14:15:39 -0400

    Be sure to export bibliography component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export { default as loadConfig } from './server/loadConfig.js';
 export { default as loadPassage } from './server/loadPassage.js';
+export { default as Bibliography } from './components/Bibliography.svelte';
 export { default as Header } from './components/Header.svelte';
 export { default as ReadingEnvironment } from './components/ReadingEnvironment.svelte';
 export * from './types.js';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@
 export { default as loadConfig } from './server/loadConfig.js';
 export { default as loadPassage } from './server/loadPassage.js';
 
+export { default as Bibliography } from './components/Bibliography.svelte';
 export { default as Header } from './components/Header.svelte';
 export { default as ReadingEnvironment } from './components/ReadingEnvironment.svelte';
 
