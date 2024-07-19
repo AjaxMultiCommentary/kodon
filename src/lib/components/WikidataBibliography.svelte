ab5de332fc135892f47f6624681c85dc543d2c0a
Charles Pletcher <pletcher@users.noreply.github.com>
2024-07-19 18:06:41 -0400

    Add sorting to WikidataBibliography

--- src/lib/components/WikidataBibliography.svelte
+++ src/lib/components/WikidataBibliography.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import type { WikidataEntry } from '$lib/types.js';
+	import { orderBy } from 'lodash';
+
+	import ArrowUp from '$lib/components/icons/ArrowUp.svelte';
+	import ArrowDown from '$lib/components/icons/ArrowDown.svelte';
 
 	export let citations: WikidataEntry[];
 
@@ -8,18 +12,87 @@
 	// "extended bibliography": show full bibliographic information, and in expandable block show who from the core corpus cites each item
 	// for extended items (Bowra, Falco, etc.): tabulate how many commentaries are citing them
 	// - sort by how many citations
+	let sortProperty: 'author' | 'pubdate' | 'title' | 'publisher' | 'place' = 'author';
+	let sortAscending = true;
+
+	$: sortedCitations = orderBy(citations, [sortProperty], [sortAscending ? 'asc' : 'desc']);
 </script>
 
 <div class="table table-pin-cols">
 	<thead>
-		<th>Author</th>
-		<th>Year of Publication</th>
-		<th>Title</th>
-		<th>Publisher</th>
-		<th>Place of Publication</th>
+		<th
+			class="cursor-pointer"
+			on:click={() => {
+				sortProperty = 'author';
+				sortAscending = !sortAscending;
+			}}
+			><div class="flex">
+				Author {#if sortProperty === 'author'}
+					{#if sortAscending}<ArrowUp className="size-4" />{:else}<ArrowDown
+							className="size-4"
+						/>{/if}
+				{/if}
+			</div></th
+		>
+		<th
+			class="cursor-pointer"
+			on:click={() => {
+				sortProperty = 'pubdate';
+				sortAscending = !sortAscending;
+			}}
+			><div class="flex">
+				Year of Publication {#if sortProperty === 'pubdate'}
+					{#if sortAscending}<ArrowUp className="size-4" />{:else}<ArrowDown
+							className="size-4"
+						/>{/if}
+				{/if}
+			</div></th
+		>
+		<th
+			class="cursor-pointer"
+			on:click={() => {
+				sortProperty = 'title';
+				sortAscending = !sortAscending;
+			}}
+			><div class="flex">
+				Title {#if sortProperty === 'title'}
+					{#if sortAscending}<ArrowUp className="size-4" />{:else}<ArrowDown
+							className="size-4"
+						/>{/if}
+				{/if}
+			</div></th
+		>
+		<th
+			class="cursor-pointer"
+			on:click={() => {
+				sortProperty = 'publisher';
+				sortAscending = !sortAscending;
+			}}
+			><div class="flex">
+				Publisher {#if sortProperty === 'publisher'}
+					{#if sortAscending}<ArrowUp className="size-4" />{:else}<ArrowDown
+							className="size-4"
+						/>{/if}
+				{/if}
+			</div></th
+		>
+		<th
+			class="cursor-pointer"
+			on:click={() => {
+				sortProperty = 'place';
+				sortAscending = !sortAscending;
+			}}
+			><div class="flex">
+				Place of Publication {#if sortProperty === 'place'}
+					{#if sortAscending}<ArrowUp className="size-4" />{:else}<ArrowDown
+							className="size-4"
+						/>{/if}
+				{/if}
+			</div></th
+		>
 	</thead>
 	<tbody>
-		{#each citations as citation}
+		{#each sortedCitations as citation}
 			<tr>
 				<td>{citation.author}</td>
 				<td>{new Date(citation.pubdate).getFullYear()}</td>
