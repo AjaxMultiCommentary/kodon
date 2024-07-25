8de7715eb5f399f9ca6f0ec9f856b8e5565c6f33
Charles Pletcher <pletcher@users.noreply.github.com>
2024-07-25 11:04:11 -0400

    Export Tooltip

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,5 +4,6 @@ export { default as loadPassage } from './server/loadPassage.js';
 export { default as Bibliography } from './components/ZoteroBibliography.svelte';
 export { default as Header } from './components/Header.svelte';
 export { default as ReadingEnvironment } from './components/ReadingEnvironment.svelte';
+export { default as Tooltip } from './components/Tooltip.svelte';
 export { default as WikidataBibliography } from './components/WikidataBibliography.svelte';
 export * from './types.js';
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,6 +7,7 @@ export { default as loadPassage } from './server/loadPassage.js';
 export { default as Bibliography } from './components/ZoteroBibliography.svelte';
 export { default as Header } from './components/Header.svelte';
 export { default as ReadingEnvironment } from './components/ReadingEnvironment.svelte';
+export { default as Tooltip } from './components/Tooltip.svelte';
 export { default as WikidataBibliography } from './components/WikidataBibliography.svelte';
 
 export * from './types.js';
